a8316664f5b82954b7991b11b5aa591bae91595f
Matheus Alencastro <alencastromatheus@gmail.com>
2022-12-13 15:39:10 -0300

    commit with text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,29 @@ export class AppComponent {
   public title = 'curriculo';
 
   public skillBars: Skill[] = [{
-    skillTitle: "Photoshop",
+    skillTitle: "C#",
     skillLevel: "90"
   },
   {
-    skillTitle: "Photography",
+    skillTitle: "SQL",
+    skillLevel: "90"
+  },
+  {
+    skillTitle: "WEB API",
     skillLevel: "80"
   },
   {
-    skillTitle: "Media",
-    skillLevel: "10"
-  }
+    skillTitle: "HTML & CSS",
+    skillLevel: "90"
+  },
+  {
+    skillTitle: "JavaScript",
+    skillLevel: "80"
+  },
+  {
+    skillTitle: "Angular",
+    skillLevel: "60"
+  },
   ]
   public languageBars: Skill[] = [{
     skillTitle: "Portuguese",
@@ -33,31 +45,31 @@ export class AppComponent {
   },
   {
     skillTitle: "English",
-    skillLevel: "80"
+    skillLevel: "90"
   },
   {
-    skillTitle: "Spanish",
-    skillLevel: "50"
+    skillTitle: "French",
+    skillLevel: "30"
   }
   ]
   public contact: ContactData = {
-    imageUrl: "https://www.w3schools.com/w3images/avatar_hat.jpg",
+    imageUrl: "./../assets/images/FOTO_PERFIL_-_Matheus_A.jpg",
     contactInfo: [{
       icon: "bi bi-person-fill",
-      text: "Fulano de Tal"},
+      text: "Matheus Alencastro"},
       {
       icon: "bi bi-wallet-fill",
-      text: "Designer"
+      text: "Full Stack Developer"
     },
     {
       icon: "bi bi-house-door-fill",
-      text: "São Paulo - SP"
+      text: "Recife - PE"
     }, {
       icon: "bi bi-mailbox2",
-      text: "teste@teste.com"
+      text: "alencastromatheus@gmail.com"
     }, {
       icon: "bi bi-telephone-fill",
-      text: "(11) 99988-7766"
+      text: "(81) 99597-4698"
     }
     ]
   }
@@ -66,14 +78,24 @@ export class AppComponent {
       icon: "bi bi-bag-fill",
       title: "Work Experience",
       experiences: [{
-        titleElement: "Front-end Developer / Safra.com",
-        textDateElement: "Jun-2015  -  Current",
-        paragElement: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        titleElement: "Full-Stack Developer / Banco Safra",
+        textDateElement: "Jun-2022  -  Dez-2022",
+        paragElement: "Contratado pelo programa Top Coders, em parceria com a Ada (antiga Let's Code). Programa consiste em treinamento Full Stack, mesclado com experiências e treinamentos internos administrados pelo Banco Safra."
       },
       {
-        titleElement: "Front-end Developer / Safra.com",
-        textDateElement: "Jun-2014  -  Jun-2015",
-        paragElement: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        titleElement: "Estagiário de Planejamento e Gestão / N&A Consultores",
+        textDateElement: "Jan-2021  -  Mai-2022",
+        paragElement: "Estágio focado no planejamento e gestão de projetos de redes solares, tendo como escopo o acompanhamento e a fiscalização da execução e entregas de serviços em obra, além de realizra o controle do faturamento na compra de materiais e em medições."
+      },
+      {
+        titleElement: "Estagiário de Qualidade / TPF Engenharia",
+        textDateElement: "Ago-2020  -  Jan-2021",
+        paragElement: "Estágio focado na fiscalização das entregas de obras de restauração, realizando sempre um controle de qualidade com a elaboração de relatórios através do Power BI."
+      },
+      {
+        titleElement: "Analista de Projetos / Poli Junior Engenharia",
+        textDateElement: "Jun-2019  -  Ago-2020",
+        paragElement: "Capacitação e elaboração de projetos de engenharia civil, utilizando tecnologias como AutoCAD e Revit, além de realizar visitas técnicas em projetos atuantes."
       }
       ]
     },
@@ -81,21 +103,26 @@ export class AppComponent {
       icon: "bi bi-mortarboard-fill",
       title: "Education",
       experiences: [{
-        titleElement: "Colegio Liceu",
-        textDateElement: "Jun-1995  -  Dez-2005",
-        paragElement: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        titleElement: "Engenharia Civil - Poli UPE",
+        textDateElement: "Jan-2018  -  Current",
+        paragElement: "Bacharelado em Engenharia Civil pela Escola Politécnica de Pernambuco (Universidade de Pernambuco)."
       },
       {
-        titleElement: "ADA",
-        textDateElement: "Jun-2022  -  Current",
-        paragElement: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        titleElement: "Formação WEB Full Stack - ADA",
+        textDateElement: "Mai-2022  -  Current",
+        paragElement: "Curso realizado através do programa Top Coders, em conjunto com o Banco Safra para formação WEB Full Stack, com foco em C# para Back-end e JS / Angular para o Front-end."
+      },
+      {
+        titleElement: "Curso Intensivo de Inglês - Language Studies International (LSI)",
+        textDateElement: "Jan-2019  -  Mar-2019",
+        paragElement: "Curso de inglês, realizado em intercâmbio em Londres, com enfoque em conversação e troca de experiências entre alunos."
       }
       ]
     }
   ];
   public socialMedias: IconText[] = [{
     icon: "bi bi-github",
-    text: "https://github.com/"
+    text: "https://github.com/matteabk"
   },
   {
     icon: "bi bi-facebook",
@@ -107,7 +134,7 @@ export class AppComponent {
   },
   {
     icon: "bi bi-linkedin",
-    text: "https://linkedin.com"
+    text: "https://www.linkedin.com/in/matheus-kenderessy/"
   }];
 
   onContactFormData (formData:FormGroup):void{
